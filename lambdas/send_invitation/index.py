f3dd6e30e7a16c4c780134a62d92cf2d0ebd40af
Daniel Renteria <daniel.silva@loanpro.io>
2026-05-11 18:56:59 -0600

    Fix send_invitation endpoint

--- lambdas/send_invitation/index.py
+++ lambdas/send_invitation/index.py
@@ -1,6 +1,7 @@
 import json
 import os
 import boto3
+from utils.response import cors_response
 
 sqs = boto3.client('sqs')
 queue_url = os.environ['QUEUE_URL']
@@ -9,31 +10,22 @@ def handler(event, context):
     try:
         subdomain = event['requestContext']['authorizer'].get('username')
         body = json.loads(event['body'])
-        
+
         confirmation_code = body.get('confirmation_code')
         if not confirmation_code:
-            return {
-                'statusCode': 400,
-                'body': json.dumps({'error': 'confirmation_code is required'})
-            }
-        
+            return cors_response(400, {'error': 'confirmation_code is required'})
+
         message = {
             'event_id': subdomain,
             'confirmation_code': confirmation_code
         }
-        
+
         sqs.send_message(
             QueueUrl=queue_url,
             MessageBody=json.dumps(message)
         )
-        
-        return {
-            'statusCode': 202,
-            'body': json.dumps({'message': 'Invitation queued for sending'})
-        }
-        
+
+        return cors_response(202, {'message': 'Invitation queued for sending'})
+
     except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
+        return cors_response(500, {'error': str(e)})
